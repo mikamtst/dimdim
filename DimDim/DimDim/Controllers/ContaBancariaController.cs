e9f21b54bbe1377a30805dde5105ca904e5bf323
Micaela Santos <micaela.santos@brlink.com.br>
2021-08-28 20:04:10 -0300

    Commit do projeto

---
+++ DimDim/DimDim/Controllers/ContaBancariaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DimDim.Context;
+using DimDim.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DimDim.Controllers
+{
+    public class ContaBancariaController : ControllerBase
+    {
+        private readonly CrudContexto _crudContexto;
+
+        public ContaBancariaController(CrudContexto contexto)
+        {
+            _crudContexto = contexto;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] ContaBancaria contaBancaria)
+        {
+            _crudContexto.ContaBancaria.Add(contaBancaria);
+            _crudContexto.SaveChanges();
+        }
+    }
+}
---
+++ DimDim/DimDim/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DimDim.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
